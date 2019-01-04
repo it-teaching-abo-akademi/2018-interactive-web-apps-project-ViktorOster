34a654f2827c3ed897be63a836d90d42758d8296
ViktorOster <Ozterkvlt@hotmail.com>
2019-01-04 12:23:57 +0200

    Updates

---
+++ precache-manifest.86367cbeeb3a0646fc0503eedf744703.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/2018-interactive-web-apps-project-ViktorOster/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "96019c068acea0826e4f",
+    "url": "/2018-interactive-web-apps-project-ViktorOster/static/js/runtime~main.96019c06.js"
+  },
+  {
+    "revision": "55180d8daa64b9e8bbda",
+    "url": "/2018-interactive-web-apps-project-ViktorOster/static/js/main.55180d8d.chunk.js"
+  },
+  {
+    "revision": "33830f9cdb83a3b2afa0",
+    "url": "/2018-interactive-web-apps-project-ViktorOster/static/js/1.33830f9c.chunk.js"
+  },
+  {
+    "revision": "55180d8daa64b9e8bbda",
+    "url": "/2018-interactive-web-apps-project-ViktorOster/static/css/main.201aa2a9.chunk.css"
+  },
+  {
+    "revision": "33830f9cdb83a3b2afa0",
+    "url": "/2018-interactive-web-apps-project-ViktorOster/static/css/1.72206804.chunk.css"
+  },
+  {
+    "revision": "971e1e0fd81a5b6169720275b2ccc51b",
+    "url": "/2018-interactive-web-apps-project-ViktorOster/index.html"
+  }
+];
